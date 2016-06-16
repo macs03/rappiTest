0f664104c4c181184b18414fca7c979dbf6942c5
Miguel Cárdenas <miguelcarde.3@gmail.com>
2016-06-15 22:41:59 -0400

    animations for the news

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,14 +12,17 @@
         .module('rappiApp')
         .controller('MainCtrl', MainCtrl);
 
-        MainCtrl.$inject = ['$scope','articlesFactory'];
+        MainCtrl.$inject = ['$scope','articlesFactory','$timeout'];
 
-        function MainCtrl($scope, articlesFactory) {
+        function MainCtrl($scope, articlesFactory, $timeout) {
             var vm = this;
             vm.callNews = callNews;
             vm.titleSelected = "";
             vm.selectNew = selectNew;
             vm.news = [];
+            vm.loadTitles = false;
+            vm.introMainTitle = false;
+            vm.outMainTitle = false;
 
             function callNews() {
                 articlesFactory
@@ -27,17 +30,33 @@
                     .then(function (data) {
                         console.log(data);
                         vm.news = data;
+                        vm.loadTitles = true;
                     })
                     .catch(function (err) {
                         console.log(err);
                     })
             }
-
+            var aux = 0;
             function selectNew(id) {
                 console.log(id);
+                if (aux == id) {
+                    vm.outMainTitle = true;
+                    console.log('cerramos');
+                    aux = 0;
+                }else{
+                    aux = id;
+                    console.log('nuevo');
+                    vm.outMainTitle = false;
+                    $('.title').removeClass('out-main-title');
+                }
                 angular.forEach(vm.news, function(value, key) {
-                    if (value.id == id) {
+                    if (value.id == id && !vm.outMainTitle) {
+                        vm.introMainTitle = true;
                         vm.titleSelected = value.title;
+                        $timeout(function () {
+                            $('.title').removeClass('intro-main-title');
+                            vm.introMainTitle = false;
+                        }, 2000);
                     }
                 });
             }
